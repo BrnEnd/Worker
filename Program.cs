ec7fdbe1dcc7165c8abe5020d208a1e058d56e92
Bruno Alexandrino <bruno.marcas@gmail.com>
2023-05-03 20:27:03 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using WorkerAgendamento.UseCase;
+
+class Program
+{
+    static bool isTaskRunning = false;
+
+    
+    static void Main(string[] args)
+    {
+
+        Timer timer = new Timer(Callback, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+        Console.ReadLine();
+    }
+
+    static void Callback(object state)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["MySql"].ConnectionString;
+        UseCaseAgendamento agendamento = new UseCaseAgendamento();
+        if (!isTaskRunning)
+        {
+            isTaskRunning = true;
+            Console.WriteLine($"Iniciando a tarefa em: {DateTime.Now}");
+
+            agendamento.ExistingNewAgendamento(connectionString);
+
+            Console.WriteLine($"A tarefa foi concluída em: {DateTime.Now}");
+            isTaskRunning = false;
+        }
+        else
+        {
+            Console.WriteLine($"A tarefa anterior ainda está em execução em: {DateTime.Now}");
+        }
+    }
+}
---
+++ UseCase/UseCaseAgendamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerAgendamento.UseCase
+{
+    public class UseCaseAgendamento 
+    {
+        public bool ExistingNewAgendamento(string connection)
+        {
+            Console.WriteLine("Chegou aqui.");
+            return true;
+        }
+    }
+}
